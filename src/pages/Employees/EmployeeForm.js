575838dd0ab89810f6aff84fc17cfcbcbc0ccc29
ltw16 <liamwebb11@hotmail.com>
2021-02-20 19:55:34 +0000

    Refactored code into reusable components, beginning to work on Department drop down selection

--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -1,8 +1,17 @@
-import { FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, TextField } from '@material-ui/core';
+import { FormControl, FormControlLabel, FormLabel, Grid, Radio, TextField } from '@material-ui/core';
 import React, {useState, useEffect} from 'react'
+import Input from '../../components/controls/Input';
+import RadioGroup from '../../components/controls/RadioGroup';
+import Select from '../../components/controls/Select';
 import { useForm, Form } from '../../components/useForm';
+import * as employeeService from '../../services/employeeService';
  
 
+const genderItems = [
+    {id: 'male', title:'Male'},
+    {id: 'female', title:'Female'},
+    {id: 'other', title:'Other'},
+]
 
 const initialFValues = {
     id: 0,
@@ -24,39 +33,39 @@ export default function EmployeeForm() {
         handleInputChange
     } = useForm(initialFValues);
 
-    
 
     return (
         <Form>
             <Grid container>
                 <Grid item xs={6}>
-                    <TextField 
-                    variant='outlined'
-                    label='Full Name'
-                    name='fullName'
-                    value={values.fullName}
-                    onChange={handleInputChange}
+                    <Input 
+                        name="fullName"
+                        label="Full Name"
+                        value={values.fullName}
+                        onChange={handleInputChange}
                     />
-                    <TextField 
-                    variant='outlined'
-                    label='Email'
-                    name='email'
-                    value={values.email}
-                    onChange={handleInputChange}
+                    <Input 
+                        label='Email'
+                        name='email'
+                        value={values.email}
+                        onChange={handleInputChange}
                     />
                 </Grid>
                 <Grid item xs={6}>
-                    <FormControl>
-                        <FormLabel>Gender</FormLabel>
-                        <RadioGroup row
-                                    name='gender'    
-                                    value={values.gender}
-                                    onChange={handleInputChange}>
-                            <FormControlLabel value="male" control={<Radio />} label="Male" />
-                            <FormControlLabel value="female" control={<Radio />} label="Female" />
-                            <FormControlLabel value="other" control={<Radio />} label="Other" />
-                        </RadioGroup>
-                    </FormControl>
+                    <RadioGroup 
+                        name='gender'    
+                        label='Gender'
+                        value={values.gender}
+                        onChange={handleInputChange}
+                        items={genderItems}
+                    />
+                    <Select
+                        name="departmentId"
+                        label="Department"
+                        value={values.departmentId}
+                        onChange={handleInputChange}
+                        options={employeeService.getDepartmentCollection()}
+                    />
                 </Grid>
             </Grid>
         </Form>
