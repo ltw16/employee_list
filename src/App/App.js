a5dad5d7e0a0187979c4c5a6bb218afe7b491a84
ltw16 <liamwebb11@hotmail.com>
2021-02-20 11:49:36 +0000

    Added basic layout and notification icons

---
+++ src/App/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import SideMenu from "../components/SideMenu";
+import Header from "../components/Header";
+import { CssBaseline, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  appMain:{
+    paddingLeft: '320px',
+    width: '100%'
+  }
+})
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <SideMenu /> 
+      <div className={classes.appMain}>
+        <Header />
+      </div>
+      <CssBaseline />
+    </>
+    
+  );
+}
+
+export default App;
